3fcbe88996ac69502a16ad111990fc601b52f6db
radhi-ops <harrarradhi@gmail.com>
2020-05-26 01:18:26 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import myImage from "./pic.jpg";
 import ProfileComponent from "./profile/ProfileComponent"
 
-
+const data =[];
 const name = "My name is radhi harrar";
 function App() {
   const handleName =  () => {
@@ -15,9 +15,11 @@ function App() {
       bio="I'm a full stack js student"
       profession="Student"
       nameAlert={handleName}
+      tasks=""
+      list={data}
     >
       <div>
-        <img src={myImage} alt="pic" width="50%" height="350px"/>
+        <img src={myImage} alt="pic" width="30%" height="250px" style={{marginBottom:"2%"}}/>
       </div>
     </ProfileComponent>
     </div>
--- src/profile/ProfileComponent.js
+++ src/profile/ProfileComponent.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import "./profile.css";
 
-const ProfileComponent = (props) => {
+const ProfileComponent = props => {
   // const { fullName, bio, profession, nameAlert } = props;
+  const [Tasks, setTasks] = useState(props.tasks);
+  const [List, setList] = useState(props.list);
+  const [Oldtask, setUpdate] = useState("");
+  const handleSubmit = event => {
+    if (Tasks) {
+      setList(List.concat(Tasks));
+    }
+    setTasks("");
+    event.preventDefault();
+  };
+
+  const handleRemove = elem => {
+    setList(List.filter(item => item !== elem));
+    setTasks("");
+  };
+
+  const handleChange = event => {
+    setTasks(event.target.value);
+  };
+
+  const handleUpdate = () => {
+    List[List.findIndex(e => e === Oldtask)] = Tasks;
+
+    setTasks("");
+  };
   return (
     <div style={{ margin: "2% 2%" }}>
-      {props.nameAlert()}
+      {/* {props.nameAlert()} */}
       <h1 style={{ textTransform: "capitalize", textDecoration: "underline" }}>
         {props.fullName}
       </h1>
@@ -28,6 +54,40 @@ const ProfileComponent = (props) => {
         My Profession : {props.profession}
       </h3>
       {props.children}
+
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={Tasks}
+            id="tasks"
+            placeholder="ADD/Remove Tasks"
+            onChange={e => handleChange(e)}
+            style={{ marginRight: "2%" }}
+          />
+          <button type="submit">Add Tasks</button>
+          <button type="button" onClick={() => handleRemove(Tasks)}>
+            Remove Tasks
+          </button>
+          <button type="button" onClick={() => handleUpdate(Tasks)}>
+            Update
+          </button>
+        </form>
+        <ul>
+          {List.map((item, i) => (
+            <li
+              className="item"
+              onClick={() => {
+                setTasks(item);
+                setUpdate(item);
+              }}
+              key={i}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
